82b804544eefed3de615bd769a56d8f5e4572801
Ali Parvizi <snparvizi75@gmail.com>
2019-11-04 21:26:44 +0330

    get data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ data }}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      data: 'hello how arae you'
+    }
+  }, mounted: function () {
+    axios.get('https://cafepay.app/api/v1/cafe/1/menu/list/', {
+      headers: {
+        Authorization: 'Token c0d98047bc1b36b7731172498781565047d45c96'
+      }
+    }).then(response => this.data = response.data)
+    .catch(error =>console.log(error));
   }
 }
 </script>
